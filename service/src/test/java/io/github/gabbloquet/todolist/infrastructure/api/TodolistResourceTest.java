74407a7b25971bc051eb4149a70d97f770ccde7f
Gabin Bloquet <gabin.blqt@gmail.com>
2022-09-18 09:56:54 +0200

    refactor: delete useless imports

--- service/src/test/java/io/github/gabbloquet/todolist/infrastructure/api/TodolistResourceTest.java
+++ service/src/test/java/io/github/gabbloquet/todolist/infrastructure/api/TodolistResourceTest.java
@@ -5,15 +5,12 @@ import io.github.gabbloquet.todolist.domain.model.Task;
 import io.github.gabbloquet.todolist.domain.model.Todolist;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 

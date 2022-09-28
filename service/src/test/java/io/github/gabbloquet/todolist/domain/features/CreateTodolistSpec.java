235066c6d872c34de4368c7a99c14cd4053bd0c8
Gabin Bloquet <gabin.blqt@gmail.com>
2022-09-28 07:45:29 +0200

    refactor: delete useless deps

--- service/src/test/java/io/github/gabbloquet/todolist/domain/features/CreateTodolistSpec.java
+++ service/src/test/java/io/github/gabbloquet/todolist/domain/features/CreateTodolistSpec.java
@@ -2,19 +2,14 @@ package io.github.gabbloquet.todolist.domain.features;
 
 import io.cucumber.java.fr.Alors;
 import io.cucumber.java.fr.Lorsque;
-import io.cucumber.java.fr.Quand;
-import io.github.gabbloquet.todolist.domain.TodolistUseCaseTransaction;
 import io.github.gabbloquet.todolist.domain.commands.OpenApplication;
-import io.github.gabbloquet.todolist.domain.repositories.TodolistRepository;
 import io.github.gabbloquet.todolist.domain.model.Todolist;
+import io.github.gabbloquet.todolist.domain.repositories.TodolistRepository;
 import org.junit.jupiter.api.Assertions;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Optional;
-import java.util.function.Supplier;
-
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
 
 public class CreateTodolistSpec {
 
@@ -23,9 +18,6 @@ public class CreateTodolistSpec {
 
     @Autowired
     private TodolistRepository todolistRepository;
-
-    @Autowired
-    private TodolistUseCaseTransaction todolistUseCaseTransaction;
 
     private final ArgumentCaptor<Todolist> todolistArgumentCaptor = ArgumentCaptor.forClass(Todolist.class);
 

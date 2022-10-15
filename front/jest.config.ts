cf953c664504b43c76803df8e58c88af3c71b0f3
Gabin Bloquet <gabin.blqt@gmail.com>
2022-10-15 08:41:36 +0200

    chore: delete useless configuration from starter

--- front/jest.config.ts
+++ front/jest.config.ts
@@ -5,7 +5,6 @@ export default {
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
-  transformIgnorePatterns: ['/node_modules/(?!nanoid)(.*)'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/../fileMock.js',

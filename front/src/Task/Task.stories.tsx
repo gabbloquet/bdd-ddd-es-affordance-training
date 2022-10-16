6c75449b156fa3744a8369fd4f87f3bbaccc7ad3
Gabin Bloquet <gabin.blqt@gmail.com>
2022-10-16 19:16:56 +0200

    chore: storybook Task Component

--- front/src/Task/Task.stories.tsx
+++ front/src/Task/Task.stories.tsx
@@ -5,7 +5,14 @@ import { Task } from './index';
 
 export default {
   title: 'Task',
-  component: Task
+  component: Task,
+  parameters: {
+    controls: {
+      matchers: {
+        date: /creationTime$/
+      }
+    }
+  }
 } as ComponentMeta<typeof Task>;
 
 const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
@@ -16,14 +23,14 @@ Default.args = {
   description: 'Allé fumé du canon avec Gégé',
   completed: false,
   creationTime: new Date('2022-09-16T07:10:00'),
-  duration: '23 heure(s) et 6 minute(s)'
+  duration: undefined
 };
 
 export const Completed = Template.bind({});
 Completed.args = {
   id: '1a8d707e-506a-4923-a688-66cbc5674f13',
   description: 'Allé fumé du canon avec Gégé',
-  completed: false,
+  completed: true,
   creationTime: new Date('2022-09-16T07:10:00'),
-  duration: undefined
+  duration: '23 heure(s) et 6 minute(s)'
 };
--- front/src/Task/task.service.ts
+++ front/src/Task/task.service.ts
@@ -1,6 +1,6 @@
 export const toLiteralDateTime = (dateTime: Date): string => {
-  const date = dateTime.toLocaleDateString('fr-FR');
-  const time = dateTime.toLocaleTimeString('fr-FR').slice(0, -3);
+  const date = new Date(dateTime).toLocaleDateString('fr-FR');
+  const time = new Date(dateTime).toLocaleTimeString('fr-FR').slice(0, -3);
 
   return `${date} à ${time}`;
 };
